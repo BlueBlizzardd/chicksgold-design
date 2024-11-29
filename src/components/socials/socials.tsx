54eddd9011f4c73de68c32f76a952a1677abdc13
BlueBlizzardd <105523256+BlueBlizzardd@users.noreply.github.com>
2024-11-29 17:07:29 -0500

    added social media icons

--- src/components/socials/socials.tsx
+++ src/components/socials/socials.tsx
@@ -4,16 +4,16 @@ export const Socials = () => {
     return (
         <div class='social-media flex'>
             <div class='icon fb flex'>
-                <img src="/facebook_white.svg" alt="facebook" />
+                <img src="/socials/facebook_white.svg" alt="facebook" />
             </div>
             <div class='icon ig flex'>
-                <img src="/instagram_white.svg" alt="instagram" />
+                <img src="/socials/instagram_white.svg" alt="instagram" />
             </div>
             <div class='icon twt flex'>
-                <img src="/twitter_white.svg" alt="twitter" />
+                <img src="/socials/twitter_white.svg" alt="twitter" />
             </div>
             <div class='icon disc flex'>
-                <img src="/discord_white.svg" alt="discord" />
+                <img src="/socials/discord_white.svg" alt="discord" />
             </div>
         </div>
     )
