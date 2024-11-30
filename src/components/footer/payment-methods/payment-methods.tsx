a31ffd0deaa61ef29c621fbb5510310b1827342a
BlueBlizzardd <105523256+BlueBlizzardd@users.noreply.github.com>
2024-11-29 20:53:51 -0500

    added payment methods row and added it to the footer

---
+++ src/components/footer/payment-methods/payment-methods.tsx
@@ -0,0 +1,20 @@
+import './payment-methods.css'
+
+export default function Payment() {
+    return (
+        <div class='payment-row'>
+            <div class='container flex'>
+                <div class='method'>
+                    <img src="/paymethods/Visa.svg" alt="Visa" />
+                </div>
+                <div class='method'>
+                    <img src="/paymethods/Mastercard.svg" alt="Master_Card" />
+                </div>
+                <div class='method'>
+                    <img src="/paymethods/Skrill.svg" alt="Skrill" />
+                </div>
+                <span>and 50+ more</span>
+            </div>
+        </div>
+    )
+}
