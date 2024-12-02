e4d69740d5bc6de382b6710d1c8f4f19730d0d52
BlueBlizzardd <105523256+BlueBlizzardd@users.noreply.github.com>
2024-12-02 15:12:14 -0500

    changed a few things about responsive mode

--- src/components/main-content/main-content.tsx
+++ src/components/main-content/main-content.tsx
@@ -1,4 +1,4 @@
-import { createResource } from 'solid-js';
+import { createMemo, createResource, createSignal } from 'solid-js';
 import { ItemGrid } from '../item-grid/item-grid';
 import { CardProps } from '../item-grid/item-card/item-card';
 import './main-content.css';
@@ -9,6 +9,8 @@ export const MainContent = () => {
         const result = await resources.json();
         return result;
     })
+
+    const doubledData = createMemo(() => data()?.concat(data()!).concat(data()!.toSpliced(2)));
 
     return (
         <main class='background'>
@@ -24,18 +26,20 @@ export const MainContent = () => {
                         <img src="/search.svg" alt="search" />
                         <input type='text' id='search' name='search' placeholder='Search' class='search-filter' />
                     </div>
-                    <div class="filter price-filter flex">
-                        <img src="https://chicksgold.com/icons/green-icons/item-bag.svg" alt="item bag" />
-                        <input type='text' id='price' name='price' placeholder='Price' class='price-filter' />
-                        <img src="/arrow_drop_down.svg" alt="arrow down" class='arrow' />
-                    </div>
-                    <div class="filter type-filter flex">
-                        <img src="https://chicksgold.com/icons/green-icons/feather.svg" alt="feather" />
-                        <input type='text' id='type' name='type' placeholder='Item Type' class='type-filter' />
-                        <img src="/arrow_drop_down.svg" alt="arrow down" class='arrow' />
+                    <div class="other-filters flex">
+                        <div class="filter price-filter flex">
+                            <img src="https://chicksgold.com/icons/green-icons/item-bag.svg" alt="item bag" />
+                            <input type='text' id='price' name='price' placeholder='Price' class='price-filter' />
+                            <img src="/arrow_drop_down.svg" alt="arrow down" class='arrow' />
+                        </div>
+                        <div class="filter type-filter flex">
+                            <img src="https://chicksgold.com/icons/green-icons/feather.svg" alt="feather" />
+                            <input type='text' id='type' name='type' placeholder='Item Type' class='type-filter' />
+                            <img src="/arrow_drop_down.svg" alt="arrow down" class='arrow' />
+                        </div>
                     </div>
                 </div>
-                <ItemGrid items={data()!} />
+                <ItemGrid items={doubledData()!} />
             </div>
         </main>
     )
